feb87554ebc27a06d743ccd7b5ec56b22bf2a36a
Denis Sleptsov <dinis68@mail.ru>
2021-12-03 23:55:37 +0300

    1

--- 3/script.js
+++ 3/script.js
@@ -1,37 +1,8 @@
-function dayOfWeek(num)
+function threeNumbers(a, b, c)
 {
-    if(num == 1)
-        return "Понедельник";
-    else if(num == 2)
-    {
-        return"Вторник";
-    }
-    else if (num == 3)
-    {  
-        return 'Среда';
-    }
-    else if(num == 4)
-    {
-        return' Четверг';
-    }
-    else if(num == 5)
-    {
-    return "Пятница";
-    }
-    else if(num == 6)
-    {
-    return"Суббота";
-    }
-    else if(num == 7)
-    {
-        return "Воскресенье";
-    {
-    else
-    {
-        return "Ошибка";
-    }
-    
+    return (a - b) / c;
 }
 
-let a = dayOfWeek(1);
-alert(a);
+let f = threeNumbers(7, 1, 2);
+alert(f);
+
---
+++ 4/script.js
@@ -0,0 +1,35 @@
+function dayOfWeek(num)
+{
+    if(num == 1)
+        return "Понедельник";
+    else if(num == 2)
+    {
+        return"Вторник";
+    }
+    else if (num == 3)
+    {  
+        return 'Среда';
+    }
+    else if(num == 4)
+    {
+        return' Четверг';
+    }
+    else if(num == 5)
+    {
+    return "Пятница";
+    }
+    else if(num == 6)
+    {
+    return"Суббота";
+    }
+    else if(num == 7)
+    {
+        return "Воскресенье";
+    {
+    else
+    {
+        return "Ошибка";
+    }    
+}
+let a = dayOfWeek(1);
+alert(a);
