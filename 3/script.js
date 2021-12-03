40ea5746f2bb741d1264a479201299c097c26a83
Denis Sleptsov <dinis68@mail.ru>
2021-12-03 23:46:28 +0300

    1

--- 3/script.js
+++ 3/script.js
@@ -1,5 +1,37 @@
-let x = function (a, b, c) {
-    return (a - b) / c;
-};
+function dayOfWeek(num)
+{
+    if(num == 1)
+        return "Понедельник";
+    else if(num == 2)
+    {
+        return"Вторник";
+    }
+    else if (num == 3)
+    {  
+        return 'Среда';
+    }
+    else if(num == 4)
+    {
+        return' Четверг';
+    }
+    else if(num == 5)
+    {
+    return "Пятница";
+    }
+    else if(num == 6)
+    {
+    return"Суббота";
+    }
+    else if(num == 7)
+    {
+        return "Воскресенье";
+    {
+    else
+    {
+        return "Ошибка";
+    }
+    
+}
 
-alert(x);
+let a = dayOfWeek(1);
+alert(a);
